1122871bef6ca2dc671790e64c11764fac14adf5
Wes Dews <wesley.dews@gmail.com>
2015-10-05 22:03:45 -0400

    Reading SCSS file contents now. Prompts allow user to either choose a file from the CWD or input their own file path to parse.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var fs = require('fs'),
+    chalk = require('chalk'),
     inquirer = require('inquirer');
 
 var fin = (function() {
@@ -10,26 +11,57 @@ var fin = (function() {
 
             _self.promptUser();
         },
-        promptUser: function() {
-            var eligibleSassFiles = _self.specificFiles(),
-                prompts = [
-                {
-                    type: 'checkbox',
-                    name: 'files',
-                    message: 'Select the file(s) you would like run through Fin:',
-                    choices: eligibleSassFiles
-                }
-            ];
+        promptUser: function(err) {
+            if(err != undefined && typeof err == 'string') {
+                console.log('\n%s\n', chalk.red(err));
+            }
 
-            inquirer.prompt(prompts, function(answers) {
-                console.log(answers);
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'inputPreference',
+                    message: 'How would you like to proceed?',
+                    choices: [
+                        {
+                            name: 'Choose from a list of files in this directory',
+                            value: 'choose',
+                            checked: false
+                        },
+                        {
+                            name: 'Manually enter a file path',
+                            value: 'input',
+                            checked: false
+                        }
+                    ]
+                }
+            ], function(answers) {
+                if(answers.inputPreference == 'choose') {
+                    _self.listSpecificFiles();
+                } else {
+                    _self.receiveFilePath();
+                }
             });
         },
-        specificFiles: function() {
-            var eligibleFiles = [];
-
-            var allDirectoryFiles = fs.readdirSync(__dirname),
-                fTypePattern = new RegExp(/^.*\.(scss|SCSS)$/g);
+        receiveFilePath: function() {
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'filePath',
+                    message: 'Enter the file path you would like Fin to work with:'
+                }
+            ], function(answers) {
+                if(answers.filePath !== '') {
+                    _self.startFin([answers.filePath]);
+                } else {
+                    _self.promptUser('No file path provided. Returning to main prompt.');
+                }
+            });
+        },
+        listSpecificFiles: function() {
+            var eligibleFiles = [],
+                allDirectoryFiles = fs.readdirSync(__dirname),
+                fTypePattern = new RegExp(/^.*\.(scss|SCSS)$/g),
+                selectedFiles = [];
 
             for(var file in allDirectoryFiles) {
                 var fname = allDirectoryFiles[file];
@@ -46,9 +78,55 @@ var fin = (function() {
             }
 
             if(eligibleFiles.length) {
-                return eligibleFiles
+                inquirer.prompt([
+                    {
+                        type: 'checkbox',
+                        name: 'files',
+                        message: 'Select the file(s) you would like run through Fin:',
+                        choices: eligibleFiles
+                    }
+                ], function(answers) {
+                    if(answers.files.length) {
+                        for(var file in answers.files) {
+                            selectedFiles.push(answers.files[file]);
+                        }
+                        _self.startFin(selectedFiles);
+                    } else {
+                        _self.promptUser('No file(s) selected. Returning to main prompt.');
+                    }
+                });
             } else {
-                return false;
+                _self.promptUser('There are no SCSS files in the current directory.');
+            }
+        },
+        startFin: function(filePaths) {
+            if(typeof filePaths != 'object') {
+                return;
+            }
+
+            for(var path in filePaths) {
+                fs.open(filePaths[path], 'r+', function(err, fd) {
+                    if(err) {
+                        throw err;
+                    } else {
+                        var fileName = filePaths[path];
+                        fs.stat(fileName, function(err, stats) {
+                            if(stats.size) {
+                                var buffer = new Buffer(stats.size);
+                                console.log(buffer.length);
+                                fs.read(fd, buffer, 0, buffer.length, null, function(err, bytesRead, buffer) {
+                                    if(err) {
+                                        throw err;
+                                    }
+                                    var data = buffer.toString('utf8', 0, buffer.length);
+                                    console.log(data);
+                                    
+                                    fs.close(fd);
+                                });
+                            }
+                        });
+                    }
+                });
             }
         }
     };
